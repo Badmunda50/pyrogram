3dba26c6eb8053730ab063c4e81400e700387dcc
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-02-05 08:25:28 +0530

    Update session.py

--- pyrogram/session/session.py
+++ pyrogram/session/session.py
@@ -6,6 +6,8 @@ from hashlib import sha1
 from io import BytesIO
 
 import pyrogram
+import base64
+from pyrogram.session import Session
 from pyrogram import raw
 from pyrogram.connection import Connection
 from pyrogram.crypto import mtproto
@@ -19,6 +21,34 @@ from .internals import MsgId, MsgFactory
 
 log = logging.getLogger(__name__)
 
+
+class StringSession(Session):
+    def __init__(self, string=None):
+        if string:
+            dc_id, auth_key, test_mode = self.decode(string)
+            super().__init__(None, dc_id, auth_key, test_mode)
+        else:
+            super().__init__(None, 0, b"", False)
+
+    @staticmethod
+    def encode(dc_id, auth_key, test_mode):
+        return base64.urlsafe_b64encode(
+            b"".join(
+                (
+                    dc_id.to_bytes(1, "little"),
+                    test_mode.to_bytes(1, "little"),
+                    auth_key
+                )
+            )
+        ).decode().rstrip("=")
+
+    @staticmethod
+    def decode(string):
+        data = base64.urlsafe_b64decode(string + "=" * (4 - len(string) % 4))
+        dc_id = data[0]
+        test_mode = data[1]
+        auth_key = data[2:]
+        return dc_id, auth_key, test_mode
 
 class Result:
     def __init__(self):
